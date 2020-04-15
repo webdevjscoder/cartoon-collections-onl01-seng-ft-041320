efa3e5e97c2d6ded206a3707b2c83322e9b0bcab
Juan Cobian <cobianjuan19@gmail.com>
2020-04-15 22:28:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,12 @@
 def roll_call_dwarves(array)# code an argument here
   order = []
-  if array.
-  number = 1
-  array.collect do |name|
-    order << "#{number}. #{name}"
-    i += 1
+  if array.empty?
+    puts "Empty!?"
+  else
+    number = 1
+    array.collect do |name|
+      order << "#{number}. #{name}"
+      i += 1
   end
   puts order
   # Your code here
