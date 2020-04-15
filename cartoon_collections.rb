62fad6e192e0e2c7150726b38a7c7af0d7420d8f
Juan Cobian <cobianjuan19@gmail.com>
2020-04-15 22:48:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planteer)# code an argument here
+
   # Your code here
 end
 
