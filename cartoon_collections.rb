dc60e52bd0a0242df6444f368c9e0698e54d571a
Juan Cobian <cobianjuan19@gmail.com>
2020-04-15 22:46:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves(array)# code an argument here
-  array.each_with_index do |name, index|
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index do |name, index|
     index += 1
     puts "#{index}. #{name}"
   end
